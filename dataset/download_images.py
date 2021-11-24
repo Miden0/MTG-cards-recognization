6a38b85ebbe6d147c8d33b4a7082df93e46a7117
Miden0 <midenoprog@gmail.com>
2021-11-24 17:40:39 -0300

    v1.0 getting images

---
+++ dataset/download_images.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+from threading import Thread
+from queue import Queue
+
+load_dotenv(".env")
+search_url = os.getenv("CARDS_SEARCH")
+
+os.chdir("original")
+
+
+def getpage(page_n, queue):
+    pointeur = page_n * 30
+    response = requests.get(f"{search_url}{pointeur}")
+    soup = BeautifulSoup(response.content, "html.parser")
+    queue.put(soup)
+
+
+def saveimg(soup, img_index):
+    title = soup.select(f"#r_main{img_index} .Card16")[0].text
+    img_url = f'http://www.mtgpics.com{soup.select(f"#r_main{img_index} a img")[0].attrs["src"]}'
+    img_url = img_url.replace("reg", "big")
+
+    img_content = requests.get(img_url).content
+
+    with open(f"{title}.jpg", "wb") as image:
+        image.write(img_content)
+
+
+total_pages = 804
+pages = Queue()
+threads = [None for i in range(4)]
+
+# get pages
+
+for p in range(3, total_pages + 1, 4):
+    print(p)
+    for i in range(4):
+        page = p - i
+        t = Thread(target=getpage, args=(page, pages))
+        t.start()
+        threads[i - 1] = t
+    for t in threads:
+        t.join()
+
+# save images
+
+threads.append(None)
+
+while not pages.empty():
+    page = pages.get()
+    print(pages.qsize())
+    for c in range(5, 31, 5):
+        for i in range(5):
+            card_index = c - i
+            t = Thread(target=saveimg, args=(page, card_index))
+            t.start()
+            threads[i - 1] = t
+        for t in threads:
+            t.join()
